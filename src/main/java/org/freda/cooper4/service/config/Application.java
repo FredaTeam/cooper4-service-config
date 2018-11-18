f220f48fa51d1956727f6e5ecd23c8f8ab70edb8
mike.1234@qq.com <mike.1234@qq.com>
2018-11-18 14:49:33 +0800

    modify register ip

--- src/main/java/org/freda/cooper4/service/config/Application.java
+++ src/main/java/org/freda/cooper4/service/config/Application.java
@@ -3,6 +3,7 @@ package org.freda.cooper4.service.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
